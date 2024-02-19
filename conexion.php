701752d11ea1de3be382ae685f6b009bd4185b84
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-02-19 02:21:42 -0500

    first commit

---
+++ conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+//1. Declaramos la variables
+$user = '';
+$pass = '';
+$host ='LAPTOP-A7ED93UJ'; //Aquí debemos poner el nombre de tu servidor (el que aparece en el sqlserver)
+$db = 'RecreoExploreIQT';
+
+//2. Realizamos el try-catch para verificar la conexión 
+try{
+//3. Realizamos la conexión
+$conexion = new PDO("sqlsrv:server=$host; database=$db", $user, $pass);
+$conexion ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo 'ERROR: '. $e->getMessage();
+}
+
+
+?>
